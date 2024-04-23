9042cf04a9cea29094e6c3e7fcbf7605657ca294
Priyanshu-Shekhar-03 <163855796+Priyanshu-Shekhar-03@users.noreply.github.com>
2024-04-23 21:24:42 +0530

    Add files via upload

--- doublepointer.cpp
+++ doublepointer.cpp
@@ -1,15 +1,39 @@
 #include<iostream>
 using namespace std;
 
+//pointer function
+void update(int **p)
+{
+//	p=p+1;
+	//kuch hoga ki nai - No
+	*p=*p +1;
+	//isme bhi kch hga ki nai -Yes
+	
+//	**p=**p+1;
+	//kya isme kuch hga - Yes
+}
+
 // double pointer
  int main(){
  	int x=5;
- 	int *p1=&x;
- 	int **p2=&p1;
+ 	int *p=&x;
+ 	int **p2=&p;
  	
- 	cout<<p1<<endl;
- 	cout<<*p1<<endl;
- 	cout<<&p1<<endl;
- 	cout<<*p2<<endl;
- 	cout<<x<<" "<<*p1<<" "<<**p2;
+// 	cout<<p1<<endl;
+// 	cout<<*p1<<endl;
+// 	cout<<&p1<<endl;
+// 	cout<<*p2<<endl;
+// 	cout<<x<<" "<<*p1<<" "<<**p2;
+
+
+cout<<"before"<< x<<endl;
+cout<<"before"<<p<<endl;
+cout<<"before"<<p2<<endl;
+
+cout<<endl<<endl;
+update (p2);
+cout<<"after"<<x<<endl;
+cout<<"after"<<p<<endl;
+cout<<"after"<<p2<<endl;
+
  }
